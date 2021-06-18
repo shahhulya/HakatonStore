53aa4ddd4a2666c0a9b61cc5fa4be80b1b9d66c4
IslamTashiev <ueaten04@gmail.com>
2021-06-18 18:03:53 +0600

    added Views, BrandPage, MainPage

---
+++ my-app/src/Views/MainPage/MainPage.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from "react";
+import HeroSlider from "../../components/HeroSlider/";
+import ProductsList from "../../components/ProductsList";
+import ProductsPagination from "../../components/ProductsPagination";
+import { storeContext } from "../../contexts/StoreContext";
+import MainLayout from "../../Layouts/MainLayout";
+import MainForm from "../../components/MainForm/MainForm";
+import bmw from "../../assets/images/bmw.jpeg";
+import bmwM5 from "../../assets/images/bmw-m5.jpeg";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
+// import HeroImg from '../../assets/images/hero.jpeg';
+// import HeroImg2 from '../../assets/images/hero2.jpeg';
+
+export default function MainPage() {
+  const { products, fetchProducts, total } = useContext(storeContext);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchProducts(page - 1);
+  }, [page]);
+
+  const heroSlider = [
+    { src: bmw, title: "bmw" },
+    { src: bmwM5, title: "bmw" },
+  ];
+
+  return (
+    <MainLayout>
+      {/* <Hero /> */}
+      <HeroSlider slider={heroSlider} />
+      <MainForm />
+      <div style={{ backgroundColor: "#fff" }}>
+        <ProductsList products={products} />
+        <ProductsPagination
+          setPage={setPage}
+          page={page}
+          count={Math.ceil(total / 4)}
+        />
+        <Footer />
+      </div>
+    </MainLayout>
+  );
+}
